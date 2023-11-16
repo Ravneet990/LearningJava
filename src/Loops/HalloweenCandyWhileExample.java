e171ab5a6d11d60b751c12ad122bc60f389e1836
ravneetkaur990 <rav95k@gmail.com>
2023-11-15 23:31:18 -0500

    Updated class name

--- src/Loops/HalloweenCandyWhileExample.java
+++ src/Loops/HalloweenCandyWhileExample.java
@@ -2,4 +2,31 @@ package Loops;
 
 public class HalloweenCandyWhileExample {
 
+	int noOfChocolates;
+
+	void trickOrTreating() {
+	
+		do {
+		
+			
+			System.out.println("Trick or Treat");
+			
+			noOfChocolates++;
+			
+			
+			
+			System.out.println("Go to the next house");
+			
+			System.out.println("Number of chocolate :" + noOfChocolates);
+			
+			
+			
+			
+		
+		}
+		
+		while (noOfChocolates<10);
+	
+	}
+
 }
